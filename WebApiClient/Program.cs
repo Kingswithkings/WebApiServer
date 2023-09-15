9817f98e982adfaa23666be2983f0bc7508e6de1
Kingswithkings <kingsuthanaidogu@gmail.com>
2023-09-15 09:02:04 +0100

    Add project files.

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("Press Any Key Cont...");
+            Console.ReadLine();
+
+
+            using(System.Net.Http.HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://localhost:44331/api/Values");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(message);
+                }
+                else
+                {
+                    Console.WriteLine($"response error code: {response.StatusCode}");
+                }
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WebApiServer/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Get() 
+        {
+            return new string[] { "Mangoe", "Bannana", "Pear", "Apple" };
+        }
+    }
+}
